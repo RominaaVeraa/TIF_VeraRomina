25145e1f49303017db15107fe2d9a9f26fc34087
RominaaVeraa <rominamicaelavera@gmail.com>
2025-11-18 23:52:46 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- preguntas.php
+++ preguntas.php
@@ -395,7 +395,7 @@
 
     <script src="js/pages/common_cart.js"></script>
     <script src="js/global.js"></script>
-    <script src="js/productos_data.php"></script>
+    <script src="js/productos_api.js"></script>
     <script src="js/pages/preguntas.js"></script>
     <script src="js/pages/index.js"></script>
     <script src="js/components.js"></script>
